4cfc7cb780a6d7b07e8edcf61e1013b7268d86ab
ankitsharma97 <ankit972125@gmail.com>
2024-06-06 12:41:02 +0530

    drf-yasg

--- api/urls.py
+++ api/urls.py
@@ -1,18 +1,30 @@
-# from django.urls import path
-# from .views import UserCreateView, UserListView, SummarizeTextView, SummarizationStatusView
-
-# urlpatterns = [
-#     path('register/', UserCreateView.as_view(), name='register'),
-#     path('users/', UserListView.as_view(), name='users'),
-#     path('summarize/', SummarizeTextView.as_view(), name='summarize'),
-#     path('summarization_status/<str:job_id>/', SummarizationStatusView.as_view(), name='summarization_status'),
-# ]
-
-
 from django.urls import path
 from .views import RegisterUser, LoginUser, LogoutUser, UserDetail, FindUsers, SummarizeText
 
+from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="APIs",
+        default_version='v1',
+        description="APIs for user and text summarization",
+        terms_of_service="",
+        contact=openapi.Contact(email="ankit972125@gmail.com"),
+        license=openapi.License(name=""),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+
+
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('login/', LoginUser.as_view(), name='login'),
     path('logout/', LogoutUser.as_view(), name='logout'),
