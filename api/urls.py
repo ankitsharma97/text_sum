29e042166b1e612021076069899a18cfae8c1e87
ankitsharma97 <ankit972125@gmail.com>
2024-06-06 16:05:37 +0530

    drf-yasg

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from .views import RegisterUser, LoginUser, LogoutUser, UserDetail, FindUsers, SummarizeText
-
-from django.urls import path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
